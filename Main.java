82a3d84e61b5539aecf619d68dcc5e04af662cb4
JeffrZephyr <jeffreysilvas09@gmail.com>
2026-07-08 08:19:18 -0700

    Added functionality to create multiple quipus

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 /********************************************
-*	AUTHOR:	
-* COLLABORATORS: 
-*	LAST MODIFIED:	
+*	AUTHOR:	Jeffrey Silvas
+* COLLABORATORS: N/A
+*	LAST MODIFIED: 7/8/2026
 ********************************************/
 
 /********************************************
@@ -11,60 +11,66 @@
 *	A user will enter a number between 0 and 999 and this program will display the number as a quipu.
 *********************************************
 *	ALGORITHM:
+ * by default, adds one to the ones place. If the value in the ones place exceeds a single digit, resets it to 0 and
+ * add one to the tens place. If the value in the tens place exceeds a single digit, resets it and add one to the
+ * hundreds place. (The maximum input is clamped at 999 so that the hundreds place never exceeds one digit.)
 *	
 *********************************************/
 
 public class Main
 {
-
-  /***** CONSTANT SECTION *****/
-
   public static void main(String[] args)
   {
     /***** DECLARATION SECTION *****/
-    int numHundreds, numTens, numOnes, number;
-
-    /***** INITIALIZATION SECTION *****/
-    numHundreds = numTens = numOnes = 0;
-    
+    int number;
+    char redo;
     
     /***** INTRO SECTION *****/
     System.out.printf("Hello! This program turns any whole number between 0 and 999 into a digital Quipu.%n%n");
-    number = UtilityBelt.readInt("Please enter a number between 0 and 999: ", 0, 999);
-    
 
     /***** INPUT SECTION *****/
-    
-
-    /***** PROCESSING SECTION *****/
-    for(int i = 0; i < number; i++) {
-      if(numOnes < 9) {
-        numOnes++;
-      } else {
-        numOnes = 0;
-        numTens++;
-      }
-      if(numTens > 9) {
-        numTens = 0;
-        numHundreds++;
-      }
-    }
+    number = UtilityBelt.readInt("Please enter a number between 0 and 999: ", 0, 999);
     
     /***** OUTPUT SECTION *****/
-    yourQuipu(numHundreds, numTens, numOnes);
+    do {
+      yourQuipu(number);
+      redo = UtilityBelt.readChar("Would you like to make another quipu? [Y/N]: ", "YN");
+    } while (redo == 'Y');
+    System.out.printf("%nGoodbye!");
     
   }
+
   /***** STATIC METHODS *****/
 
   /**
    * Returns a quipu with the specified amount of hundreds, tens, and ones
    *
-   * @param hundreds The amount of beads in the hundreds place
-   * @param tens The amount of beads in the tens place
-   * @param ones The amount of beads in the ones place
+   * @param number The number to be written out on the quipu
    *
    * @author Jeffrey Silvas jeffreysilvas09@gmail.com**/
-  static void yourQuipu(int hundreds, int tens, int ones) {
+  static void yourQuipu(int number) {
+    int hundreds, tens, ones;
+    hundreds = tens = ones = 0;
+
+    //Separates number into ones, tens, and hundreds places
+    for(int i = 0; i < number; i++) {
+      if(ones < 9) {
+        ones++;
+      } else {
+        ones = 0;
+        tens++;
+      }
+      if(tens > 9) {
+        tens = 0;
+        hundreds++;
+      }
+    }
+
+    System.out.printf("%-11s= %d%n", "Hundreds", hundreds);
+    System.out.printf("%-11s= %d%n", "Tens", tens);
+    System.out.printf("%-11s= %d%n%n", "Ones", ones);
+
+    //Prints quipu to console
     System.out.println("Your quipu:");
     UtilityBelt.printCentered(30, "___");
     UtilityBelt.printCentered(30, "|");
